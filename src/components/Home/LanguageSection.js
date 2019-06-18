099715b097a8ea3f0f01509c3a686f84ba53e202
Amit <amit.kumar@catalyst.sh>
2019-06-18 12:43:06 +0530

    Skills Section development done

--- src/components/Home/LanguageSection.js
+++ src/components/Home/LanguageSection.js
@@ -1,28 +1,54 @@
 import React from 'react'
         export default class LanguageSection extends React.Component {
-    render() {
+        render() {
         return (
-                <div className="language-design-section section  bg align-items-center">
-                    <div className="container-fluid">
-                        <div className="row ">
-                            <div className="col-sm-6 col-12 d-flex align-items-center">
-                                <img src="img/Programming-languages-display.png" />
-                            </div>
-                            <div className="col-sm-6 col-12">
-                                <h2>Programming languages we work in:</h2>
-                                <div className="language-wrapper">
-                                    <h5>Javascript Frameworks</h5>
-                                    <li></li>
-                                    <li></li>
-                                    <li></li>
-                                    <li></li>
-                                    <li></li>
-                                    <li></li> 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+<div className="language-design-section section  bg align-items-center">
+    <div className="container-fluid">
+        <div className="row ">
+            <div className="col-sm-6 col-12 d-flex align-items-center">
+                <img src="img/Programming-languages-display.png" />
+            </div>
+            <div className="col-sm-6 col-12">
+                <h2>Programming languages we work in:</h2>
+                <div className="language-wrapper">
+                    <h5>Javascript Frameworks</h5>
+                    <li>
+                        <img  className="wp-image-705 size-full" src="img/skills/typescript.png" alt=""  />
+                        <span>Typescript</span>
+                    </li>
+                    <li>
+                        <img  className="wp-image-705 size-full" src="img/skills/logo.png" alt=""  />
+                        <span>Angular Js</span>
+                    </li>
+                    <li>
+                        <img  className="wp-image-705 size-full" src="img/skills/download-1.png" alt=""  />
+                        <span>Vue Js</span>
+                    </li>
+                    <li>
+                        <img  className="wp-image-705 size-full" src="img/skills/React.js_logo-512.png" alt=""  />
+                        <span>React Js</span>
+                    </li>
+                    <li>
+                        <img  className="wp-image-705 size-full" src="img/skills/download-2.png" alt=""  />
+                        <span>Vuetify JS</span>
+                    </li>
+                    <li>
+                        <img  className="wp-image-705 size-full" src="img/skills/logo-coralspace-left.png" alt=""  />
+                        <span>Meteor node JS</span>
+                    </li>
+                    <li>
+                        <img  className="wp-image-705 size-full" src="img/skills/download.png" alt=""  />
+                        <span>Gatsby JS</span>
+                    </li>
+                    <li>
+                        <img  className="wp-image-705 size-full" src="img/skills/1200px-Node.js_logo.svg_.png" alt=""  />
+                        <span>Node JS</span>
+                    </li>
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
                 )
-    }
-}
+        }
+        }
